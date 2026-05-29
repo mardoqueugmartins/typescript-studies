561b8c010a831029afe2077ae0e390e1879381c5
Mardoqueu Martins <dev.mardoqueumartins@gmail.com>
2026-05-29 12:13:32 -0300

    First commit

---
+++ 01_fundamentals/static_vs_dynamic_typing.ts
@@ -0,0 +1,23 @@
+// Dynamic //
+
+// let age;
+
+// age = 21;
+// typeof age // number
+// console.log(age)
+
+// age = '21';
+// typeof age // string
+// console.log(age)
+
+// Static //
+
+let age: number;
+
+age = 21;
+typeof age // number
+console.log(age)
+
+age = '21'; 
+typeof age // Error: age must be of type number
+console.log(age)
---
+++ README.md
@@ -0,0 +1,6 @@
+# TypeScript Studies
+
+Repository created to study TypeScript.
+
+## Topics studied
+- Dynamic vs Static Typing
